782bcfa5f98a9aeb4e4bb0f6d6a6e82ce417bcbd
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-14 10:03:51 +0200

    Initial commit

---
+++ src/main/java/com/nnamdi/noteapp/NoteTakingAppApplication.java
@@ -0,0 +1,13 @@
+package com.nnamdi.noteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteTakingAppApplication.class, args);
+	}
+
+}
