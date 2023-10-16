790bdac0698fbc536b9c5d655998873a798eb328
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-16 10:28:15 +0200

    Implemented AbstractBaseEntity to provide blueprints for other models regarding common fields

---
+++ src/main/java/com/nnamdi/noteapp/model/AbstractBaseEntity.java
@@ -0,0 +1,55 @@
+package com.nnamdi.noteapp.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+@Slf4j
+@Data
+@MappedSuperclass
+public class AbstractBaseEntity implements Serializable {
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private String id;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private ZonedDateTime lastModifiedDate;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", length = 40)
+    private String lastModifiedBy;
+
+    @CreatedDate
+    @Column(name = "created_date")
+    private ZonedDateTime createdDate;
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, length = 40)
+    private String createdBy;
+
+    @PrePersist
+    public void  abstractPrePersist() {
+        log.debug("about to run abstractPrePersist method");
+        createdDate = ZonedDateTime.now();
+        lastModifiedDate = ZonedDateTime.now();
+        log.debug("finished running abstractPrePersist method ");
+    }
+
+    @PreUpdate
+    public void abstractPreUpdate() {
+        lastModifiedDate = ZonedDateTime.now();
+    }
+
+
+
+}
