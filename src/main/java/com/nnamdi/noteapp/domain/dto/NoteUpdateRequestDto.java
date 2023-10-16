bf61878432d8832b5e3785f13937af61701bc97d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-16 10:42:05 +0200

    Implemented NoteUpdateRequestDto

---
+++ src/main/java/com/nnamdi/noteapp/domain/dto/NoteUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.nnamdi.noteapp.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoteUpdateRequestDto {
+    @NotBlank(message = "content must be provided")
+    @Size(max = 5000, message = "content must be less than 5000 characters")
+    private String content;
+
+    @NotBlank(message = " author of the update must be provided")
+    private String author;
+
+
+}
