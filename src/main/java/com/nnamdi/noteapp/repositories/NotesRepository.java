11f9413041cffe3a0b4fc28e8b606305f01b1261
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-17 16:50:53 +0200

    chore: Added query to fetch notes by author name

--- src/main/java/com/nnamdi/noteapp/repositories/NotesRepository.java
+++ src/main/java/com/nnamdi/noteapp/repositories/NotesRepository.java
@@ -1,7 +1,10 @@
 package com.nnamdi.noteapp.repositories;
 
 import com.nnamdi.noteapp.model.Notes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +12,7 @@ import java.util.Optional;
 @Repository
 public interface NotesRepository extends JpaRepository<Notes,String> {
     Optional<Notes> findByTitle(String title);
+
+    @Query("select n from Notes n where n.createdBy = %?1% order by n.createdDate desc")
+    Page<Notes> findByAuthor(String author, Pageable pageable);
 }
